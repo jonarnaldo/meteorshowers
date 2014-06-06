3052f0e5dfd66ae1b2ebeb14a2f1340c3ac60f15
jonarnaldo <jonathanarnaldo@Jonathans-MacBook-Air.local>
2014-06-06 16:51:57 -0700

    Load updated data on page refresh

--- Client/main.js
+++ Client/main.js
@@ -1,88 +1,61 @@
-Address = {
-  display_name: "",
-  cityStateZip: "",
-  lat: 0,
-  lon: 0,
-  prev: "" 
-};
+MS = {
+  Round: function (num) { 
+    return Math.round(num);  
+  },
+  GetCurrentDay: function (unix) {
+    var currentDateArr = new Date(unix * 1000).toDateString().split(" ");
+    return currentDateArr[0];
+  },
+  GetNominatim: function (stringval) {
+    var nomdeferred = Q.defer();  
+    var baseURL = "http://nominatim.openstreetmap.org/search?format=json&limit=3&q=";
+    Session.set('stringval', stringval);
+    console.log(baseURL + stringval);
+    $.getJSON(baseURL + stringval, nomdeferred.resolve);
+    return nomdeferred.promise;
+  },
+  SetLatLon: function (data) { 
+    var searchRes = {}, dataArr = [];
+    for (var i = 0; i < data.length; i++) {
+      searchRes[i] = data[i];
+      dataArr.push(data[i].display_name);
+    }
 
-
-Round = function (num) { 
-  return Math.round(num);  
-};
-
-GetCurrentDay = function (unix) {
-  var currentDateArr = new Date(unix * 1000).toDateString().split(" ");
-  return currentDateArr[0];
-};
-
-//------ Move above up go Global Object ----------//
-
-GetNominatim = function (stringval) {
-  var nomdeferred = Q.defer();  
-  var baseURL = "http://nominatim.openstreetmap.org/search?format=json&limit=3&q=";
-  Session.set('stringval', stringval);
-  $.getJSON(baseURL + stringval, nomdeferred.resolve);
-  return nomdeferred.promise;
-};
-
-//this is called after GetNominatim is resolved
-
-SetLatLon = function (data) { 
-  var searchRes = {}, dataArr = [];
-  for (var i = 0; i < data.length; i++) {
-    searchRes[i] = data[i];
-    dataArr.push(data[i].display_name);
-  }
-
-  Session.set('DataArr',dataArr); //for jquery autocomplete
-  Session.set('SearchRes',searchRes); 
-//break up into separate function
-  var currentSearch = Session.get('SearchRes');
-  var stringval = Session.get('stringval');
-  for (var key in currentSearch) {
-    var obj = currentSearch[key];
-    for (var prop in obj) {
-      if (obj[prop] == stringval) {
-        Session.set('lat', obj.lat);
-        Session.set('lon', obj.lon); 
-      }
-    } 
-  }
-};
-
-//call after SetLatLon is resolved
-SetCoordinates = function (){
-  var currentSearch = Session.get('SearchRes');
-  var stringval = Session.get('stringval');
-  for (var key in currentSearch) {
-    var obj = currentSearch[key];
-    for (var prop in obj) {
-      if (obj[prop] == stringval) {
-        Session.set('lat', obj.lat);
-        Session.set('lon', obj.lon); 
-      }
-    } 
-  }
-}
-
-GoogleReverseLookup = {
-  setAddress: function () {
+    Session.set('DataArr',dataArr); //for jquery autocomplete
+    Session.set('SearchRes',searchRes); 
+  },
+  //call after SetLatLon is resolved
+  SetCoordinates: function (){
+    var currentSearch = Session.get('SearchRes');
+    var stringval = Session.get('stringval');
+    for (var key in currentSearch) {
+      var obj = currentSearch[key];
+      for (var prop in obj) {
+        if (obj[prop] == stringval) {
+          Session.set('lat', obj.lat);
+          Session.set('lon', obj.lon); 
+        }
+      } 
+    }
+  },
+  GoogleReverseLookup: function () {
     var googdeferred = Q.defer();
 
-
-    var lat = Session.get('lat');
-    var lon = Session.get('lon');
+    var lat = Session.get('lat'), lon = Session.get('lon');
     var APIkey =  "AIzaSyDb7R_MQT2cX6vHsbFRoweKMiM9KvocxWM",
     URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&sensor=true_or_false&key=";
 
     $.getJSON(URL + APIkey, googdeferred.resolve);
     return googdeferred.promise;
-  }
-};
-
-if (Meteor.isClient) {
-  GetLatestWeather = function (latitude, longitude) {
+  },
+  Address: {
+    display_name: "",
+    cityStateZip: "",
+    lat: 0,
+    lon: 0,
+    prev: "" 
+  },
+  GetLatestWeather: function (latitude, longitude) {
     var forecastdeferred = Q.defer();
     var apiKey = "d1d066174bbcfed66db995ace8e1b671";
     var url = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude + "?callback=?";
@@ -90,8 +63,25 @@ if (Meteor.isClient) {
 
     $.getJSON(url, forecastdeferred.resolve);
     return forecastdeferred.promise;
+  },
+  updateCurrent: function() {
+    return function(callback){
+      var buffer = null;
+      buffer = Current.find({}).fetch();
+      callback(buffer);
+    }
   }
-}; 
+};
+
+if (Meteor.isClient) {
+  Meteor.startup(function(){
+    Deps.autorun(function() {
+      console.log('Meteorshowers start up');
+      var foo = MS.updateCurrent();
+      foo(function(data){ console.log(data); });
+    });
+  });
+}
 
 Meteor.methods({
 
@@ -105,25 +95,28 @@ Meteor.methods({
       cityStateZip: cityStateZip,
       lat: lat,
       lon: lon,
-      temperature: Round(obj.temperature),
-      apparentTemperature: Round(obj.apparentTemperature),
+      temperature: MS.Round(obj.temperature),
+      apparentTemperature: MS.Round(obj.apparentTemperature),
       humidity: obj.humidity,
       windspeed: obj.windSpeed,
       visibility: obj.visibility,
       icon: obj.icon,
-      summary: obj.summary,   
+      summary: obj.summary,
+      precipProbability: obj.precipProbability,
     });
   },
 
   insertFuture: function(dailyArr){
     for (var i = 1; i < 5; i++) {
       Future.insert({  //refactor to .update instead of .insert
-        day: GetCurrentDay(dailyArr[i].time),
+        day: MS.GetCurrentDay(dailyArr[i].time),
         icon: dailyArr[i].icon,
-        temperatureMin: Round(dailyArr[i].temperatureMin),
-        temperatureMax: Round(dailyArr[i].temperatureMax),
-        apparentTemperatureMin: Round(dailyArr[i].apparentTemperatureMin),
-        apparentTemperatureMax: Round(dailyArr[i].apparentTemperatureMax),
+        temperatureMin: MS.Round(dailyArr[i].temperatureMin),
+        temperatureMax: MS.Round(dailyArr[i].temperatureMax),
+        apparentTemperatureMin: MS.Round(dailyArr[i].apparentTemperatureMin),
+        apparentTemperatureMax: MS.Round(dailyArr[i].apparentTemperatureMax),
+        summary: dailyArr[i].summary,
+        precipProbability: dailyArr[i].precipProbability,
       });
     }    
   }
--- Client/views/nav.js
+++ Client/views/nav.js
@@ -1,88 +1,55 @@
 if (Meteor.isClient) {
- 
-  Template.nav.events({
 
+  Meteor.subscribe('current');
+
+  Template.nav.events({
     //user should activate after they press enter to submit search
-    
     'mouseover #input': function () {
       $('#input').autocomplete({
         source: function(request,response){
-          response(Address.prev);
+          response([foo]); //change to reactive source
         },
-        delay: 0,
+        delay: 300,
         minLength: 4
       });
 
       $('#input').autocomplete('search'," ");
 
-      $('#input').autocomplete({
-        source: function(request,response) {
-          response(Address.prev);
-        },
-        select: function(even,ui){
-          Session.set('previousSearch', ui);
-          console.log('autocomplete hover: ' + ui.item.value);
-          Address.display_name = ui.item.label;
+      $('#input').click(function () {
+        $('#input').autocomplete({
+          source: function(request,response) {
+            MS.GetNominatim(request.term).then(function(data){
+              MS.SetLatLon(data);
+              response(Session.get('DataArr'));
+            }).done();
+          },
+          select: function(even,ui){
+            Session.set('previousSearch', ui.item.label);
+            Session.set('currentSearch', ui.item.label);
+            MS.Address.display_name = ui.item.label;
 
-          GetNominatim(Session.get('currentSearch')).then(function(data){
-            SetLatLon(data);
-            SetCoordinates();
-          }).then(function() {
-            return GoogleReverseLookup.setAddress();
-          }).then(function(googData){
-            Session.set('cityStateZip',googData.results[4].formatted_address);
-          }).then(function() {
-            Address.cityStateZip = Session.get('cityStateZip');
-            Address.lat = Session.get('lat');
-            Address.lon = Session.get('lon');
-            Meteor.call('removeData');
-            return GetLatestWeather(Session.get('lat'),Session.get('lon'));
-          }).then(function(weatherData){
-            Meteor.call('insertCurrent',weatherData.currently,Session.get('cityStateZip'),Session.get('lat'),Session.get('lon'));
-            Meteor.call('insertFuture', weatherData.daily.data);
-          }).done();
-          $(this).val('');
-          return false;
-        }
-          
-      });
-    }, 
-
-    'click #input': function () {
-      console.log('clicked');
-      $('#input').autocomplete({
-        source: function(request,response){
-          GetNominatim(request.term).then(function(data){
-            SetLatLon(data);
-          }).then(function(){
-            response(Session.get('DataArr'));
-          }).done();           
-        },
-        select: function(event,ui){
-          Address.prev = ('previousSearch', ui);
-          Session.set('currentSearch', ui.item.label);
-
-          GetNominatim(Session.get('currentSearch')).then(function(data){
-            SetLatLon(data);
-            SetCoordinates();
-          }).then(function() {
-            return GoogleReverseLookup.setAddress();
-          }).then(function(googData){
-            Session.set('cityStateZip',googData.results[4].formatted_address);
-          }).then(function() {
-            Address.cityStateZip = Session.get('cityStateZip');
-            Address.lat = Session.get('lat');
-            Address.lon = Session.get('lon');
-            Meteor.call('removeData');
-            return GetLatestWeather(Session.get('lat'),Session.get('lon'));
-          }).then(function(weatherData){
-            Meteor.call('insertCurrent',weatherData.currently,Session.get('cityStateZip'),Session.get('lat'),Session.get('lon'));
-            Meteor.call('insertFuture', weatherData.daily.data);
-          }).done();
-          $(this).val('');
-          return false;
-        }
-      });
+            MS.GetNominatim(Session.get('currentSearch')).then(function(data){
+                MS.SetLatLon(data);
+                MS.SetCoordinates();
+              }).then(function() {
+                return MS.GoogleReverseLookup();
+              }).then(function(googData){
+                Session.set('cityStateZip',googData.results[4].formatted_address);
+              }).then(function() {
+                MS.Address.cityStateZip = Session.get('cityStateZip');
+                MS.Address.lat = Session.get('lat');
+                MS.Address.lon = Session.get('lon');
+                Meteor.call('removeData');
+                return MS.GetLatestWeather(Session.get('lat'),Session.get('lon'));
+              }).then(function(weatherData){
+                Meteor.call('insertCurrent',weatherData.currently,Session.get('cityStateZip'),Session.get('lat'),Session.get('lon'));
+                Meteor.call('insertFuture', weatherData.daily.data);
+              }).done();
+              $(this).val('');
+              return false;
+          }
+        });
+      });  
     }
-  }); 
+  });
 } 
